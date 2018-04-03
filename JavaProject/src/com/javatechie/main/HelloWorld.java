8112fed25e1992bc44cc05ae45ab53a28f13ac40
vipassa25 <vipassanand2567@gmail.com>
2018-04-03 20:24:02 +0530

    first commit

---
+++ JavaProject/src/com/javatechie/main/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.javatechie.main;
+
+public class HelloWorld {
+	public static void main(String args[])
+	{
+		int data=50/0;
+	      System.out.println("rest of the code...");  
+		System.out.println("Hello World.....!!!!");
+		
+		String str = null;
+		if(str.length()==1){
+			System.out.println("test");
+		}
+	}
+
+}
